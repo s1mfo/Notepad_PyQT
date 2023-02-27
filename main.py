705e8979ac866604a55aeea47ebb9f72de1a547d
Jeongbo Sim <imfo3842@gmail.com>
2023-02-27 22:54:05 +1000

    Notepad

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from PyQt5.QtWidgets import QApplication, QTextBrowser, QLineEdit, QVBoxLayout, QWidget
 from PyQt5.QtCore import pyqtSignal
 import sys
+import time
 
 class Window(QWidget) :
+    
     def __init__ (self):
         super().__init__()
 
         self.title="PyQt5 Simple Application"
         self.top = 400
         self.left = 400
-        self.width = 800
+        self.width = 600
         self.height = 600
 
         self.setWindowTitle(self.title)
@@ -20,7 +22,7 @@ class Window(QWidget) :
     
     def Ui (self):
         self.browser = QTextBrowser()
-        self.lineEdit = QLineEdit("Calculator!")
+        self.lineEdit = QLineEdit()
 
         vbox = QVBoxLayout()
         vbox.addWidget(self.browser)
@@ -28,16 +30,25 @@ class Window(QWidget) :
 
         self.setLayout(vbox)
         self.lineEdit.returnPressed.connect(self.updateBrowser)
+        
     
     def updateBrowser(self):
+        now = time
+        now_time = now.strftime('%H:%M:%S')
+        insert_line = "-------------------------------------------------------------------------------------------"
+
         try:
             text = str(self.lineEdit.text ())
             self.browser.append("%s = <b>%s</b>" %(text, eval(text)))
+            self.lineEdit.clear()
         except:
-            self.browser.append(
-                "<font - color = red> <b>%s</b> is invalid! Type number only </font>" %text
-            )
-
+            if text=="--":
+                self.browser.append(insert_line)
+                self.lineEdit.clear()
+            else:
+                self.browser.append("<b>[%s]</b> %s" %(now_time, text))
+                self.lineEdit.clear()
+          
 app = QApplication(sys.argv)
 window = Window()
 window.show()
