9cf536a1021cc04fdcf9004ce790008a985a10e6
Jeongbo Sim <imfo3842@gmail.com>
2023-09-24 21:43:54 +1000

    Change to line Edit, Adding Save Button

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from PyQt5.QtWidgets import QApplication, QTextBrowser, QLineEdit, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QApplication, QLineEdit, QVBoxLayout, QWidget, QPushButton, QTextEdit
 from PyQt5.QtGui import QIcon, QFont
 import sys
 import time
 
+now = time
+now_time = now.strftime('%H:%M:%S')
+now_date = now.localtime().tm_mday
+now_month = now.localtime().tm_mon
+now_year = now.localtime().tm_year
 
 class Window(QWidget) :
     
@@ -23,41 +28,44 @@ class Window(QWidget) :
     
     def Ui (self):
         
-        self.browser = QTextBrowser()
+        self.edit = QTextEdit()
         self.lineEdit = QLineEdit()
+        self.save_btn = QPushButton('Save')
+        self.save_btn.clicked.connect(self.save_text)
 
         vbox = QVBoxLayout()
-        vbox.addWidget(self.browser)
+        vbox.addWidget(self.edit)
         vbox.addWidget(self.lineEdit)
+        vbox.addWidget(self.save_btn)
 
         self.setLayout(vbox)
         self.lineEdit.returnPressed.connect(self.updateBrowser)
-        
     
+
+
     def updateBrowser(self):
-        now = time
-        now_time = now.strftime('%H:%M:%S')
-        now_date = now.localtime().tm_mday
-        now_month = now.localtime().tm_mon
-        now_year = now.localtime().tm_year
-        
+       
         try:
             text = str(self.lineEdit.text ())
-            self.browser.append("%s = <b>%s</b>" %(text, eval(text)))
+            self.edit.append("<b>[%s]</b> %s = <b>%s</b>" %(now_time, text, eval(text)))
             self.lineEdit.clear()
             
         except:
             if text == "*start":
-                self.browser.append("<p style='font-size:16px'> <b>%s/%s/%s</b></p>" %(now_date,now_month,now_year))
+                self.edit.append("<p style='font-size:16px'> <b>%s/%s/%s</b></p>" %(now_date,now_month,now_year))
                 self.lineEdit.clear()
             elif text == "--":
                 insert_line = "-------------------------------------------------------------------------------------------"
-                self.browser.append("<p style= 'color:blue'>%s</p>"%(insert_line))
+                self.edit.append("<p style= 'color:blue'>%s</p>"%(insert_line))
                 self.lineEdit.clear()
             else:
-                self.browser.append("<b>[%s]</b> %s" %(now_time, text))
+                self.edit.append("<b>[%s]</b> %s" %(now_time, text))
                 self.lineEdit.clear()
-                
+
+    def save_text(self):
+        with open('test.txt', 'a' ) as f:
+            my_text = self.edit.toPlainText()
+            f.write(my_text)               
           
 app = QApplication(sys.argv)
 window = Window()
